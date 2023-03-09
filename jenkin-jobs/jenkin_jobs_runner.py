79c806f2673bedae3296274ce3a5394ebd0fe669
harrington ricks <harrington.ricks@hcl.com>
2023-03-09 09:56:11 -0500

    modify jenkin job runner for linux and window

--- jenkin-jobs/jenkin_jobs_runner.py
+++ jenkin-jobs/jenkin_jobs_runner.py
@@ -1,2 +1,18 @@
 import os
-os.system('jenkins-jobs --conf jenkins_jobs.ini update my-job')
+
+# Get the current operating system
+if os.name == "nt":
+    # Windows environment
+    jenkins_jobs_cmd = "jenkins-jobs.bat"
+else:
+    # Linux environment
+    jenkins_jobs_cmd = "jenkins-jobs"
+
+# Build the command string
+command = f"{jenkins_jobs_cmd} --conf jenkins_jobs.ini update my-job"
+
+# Run the command
+try:
+    os.system(command)
+except Exception as e:
+    print("Error running command:", e)
