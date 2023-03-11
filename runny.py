4c7d060eb838a52fc858e9c965cad373c799f322
harrington ricks <harrington.ricks@hcl.com>
2023-03-11 00:35:32 -0500

    modify runny.py

--- runny.py
+++ runny.py
@@ -1,2 +1,19 @@
-from robot import run 
-run('C:\Users\harrington\project2020\demo-robotframework\test_robot_example_2.robot')
+import os
+import subprocess
+
+# Set the path to the Robot Framework script you want to run
+ROBOT_SCRIPT_PATH = "WORKSPACE/runny.py"
+
+# Set the path to the output file for the Robot Framework results
+ROBOT_OUTPUT_PATH = "WORKSPACE/output.xml"
+
+# Set any additional Robot Framework command line options
+ROBOT_OPTIONS = "--variable ENV:prod"
+
+# Change to the directory where your Robot Framework script is located
+os.chdir(os.path.dirname(ROBOT_SCRIPT_PATH))
+
+# Run the Robot Framework script using subprocess
+subprocess.check_call(
+    ["robot", ROBOT_OPTIONS, "--output", ROBOT_OUTPUT_PATH, ROBOT_SCRIPT_PATH]
+)
